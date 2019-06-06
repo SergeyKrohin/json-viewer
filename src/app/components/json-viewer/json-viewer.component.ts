dd629646e63c126fe756e57766d574a082569169
sergey.krohin <sergey.krohin@viaccess-orca.com>
2019-06-06 15:47:44 +0300

    setting the layout for the json-viewer

--- src/app/components/json-viewer/json-viewer.component.ts
+++ src/app/components/json-viewer/json-viewer.component.ts
@@ -7,7 +7,37 @@ import { Component } from '@angular/core';
 })
 
 export class JsonViewerComponent{
-	public sourceObj = '';
+	public sourceObj = {
+		"a": "Hello, World!",
+		"x": true,
+		"b": {
+			"c": {
+				"d": {
+					"e": 123,
+					"f": true,
+					"g": "A string."
+				}
+			}
+		},
+		"h": "1",
+		"i": true,
+		"j": {
+			"k": {
+				"l": "Test!",
+				"m": true,
+				"n": false,
+				"o": {
+					"p": 987,
+					"q": 5,
+					"r": 1,
+					"s": {
+						"t": true,
+						"u": "This is where it ends."
+					}
+				}
+			}
+		}
+	};
 	
 	public validate() {
 		console.log('validating..', this.sourceObj);
